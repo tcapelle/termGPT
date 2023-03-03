9ed30893a8f2e2a254e1ead6d733e9afd4180525
Thomas Capelle <tcapelle@pm.me>
2023-03-03 16:50:15 +0100

    first version

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+exec(open('termgpt/version.py').read())
+
+setup(
+  name = 'termgpt',
+  packages = find_packages(),
+  version = __version__,
+  license='MIT',
+  description = 'A chatGPT client on the terminal',
+  author = 'Thomas Capelle',
+  author_email = 'tcapelle@pm.me',
+  url = 'https://github.com/tcapelle/termgpt',
+  long_description_content_type = 'text/markdown',
+  keywords = [
+    'artificial intelligence',
+    'generative models',
+    'natural language processing',
+    'openai',
+  ],
+  install_requires=[
+    'rich',
+    'openai',
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.10',
+  ],
+  entry_points={"console_scripts": ["gpt=termgpt.chat:main"]},
+)
