62cb8fc4c946bfc292585ed657f3aba0d51628f6
Thomas Capelle <tcapelle@pm.me>
2023-03-06 09:39:28 +0100

    refactor with history resume

--- termgpt/chat.py
+++ termgpt/chat.py
@@ -1,4 +1,4 @@
-import os, argparse
+import os, argparse, json, atexit
 
 import openai
 
@@ -13,31 +13,62 @@ if not openai.api_key:
     console.print("[bold red]Please set OPENAI_API_KEY environment variable[/]")
     exit(1)
 
-history = [
-    {"role": "system", "content": "You are a helpful assistant."},
-]
-
-
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("file", type=str, nargs="?", default=None, help="File to read [optional]")
+    parser.add_argument("-r", "--resume", action="store_true", help="Resume previous session")
     return parser.parse_args()
 
 
-args = parse_args()
-if args.file:
-    with open(args.file, "r") as f:
-        history.append({"role": "user", "content": "I will ask question about this file" + f.read()})
+class Chat:
+    """Class to handle chat with chatGPT, supports history and load from file"""
 
-def main():
-    while q := console.input("[bold red]> [/]"):
-        history.append({"role": "user", "content": q})
+    def __init__(self, file=None, resume=False):
+        self.history_file = "chatgpt_history.json"
+        self.history = [{"role": "system", "content": "You are a helpful assistant."}, ]
+        if resume:
+            with open(self.history_file, "r") as f:
+                self.history = json.load(f)
+                self._print_history(self.history)
+        if file:
+            with open(file, "r") as f:
+                self.history.append({"role": "user", "content": "I will ask question about this file" + f.read()})
 
+    def _print_history(self, history):
+        console.print("--Resuming previous session--")
+        for h in history:
+            if h["role"] == "system":
+                console.print(Text("[System] " + h["content"] +"\n", style="bold green"))
+            elif h["role"] == "user":
+                console.print("[bold red]> [/]" + h["content"])
+            else:
+                console.print(Text(h["content"], style="bold green"))
+
+    def add(self, role, content):
+        self.history.append({"role": role, "content": content})
+    
+    def __call__(self, question):
+        self.add("user", question)
         r = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            messages=history,
+            messages=self.history,
         )
         out = r["choices"][0]["message"]["content"]
-        formated_out = Text(out, justify="right")
-        history.append({"role": "assistant", "content": out})
-        console.print(f"\n[bold green]{out}[/]\n")
+        self.add("assistant", out)
+        console.print(Text(out, style="bold green"))
+
+    def input(self):
+        q = console.input("[bold red]> [/]")
+        return q
+
+    def save(self):
+        with open(self.history_file, "w") as f:
+            json.dump(self.history, f)
+
+
+def main():
+    args = parse_args()
+    chat = Chat(file=args.file, resume=args.resume)
+    atexit.register(chat.save)
+    while q := chat.input():
+        chat(q)
